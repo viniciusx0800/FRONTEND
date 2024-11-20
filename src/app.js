c8fbbb2dd182f6d85688688b2d10ad3106399deb
Viniciusx0800 <vinioliveira2008y@gmail.com>
2024-11-20 20:25:42 -0300

    feat: add conteudo

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 const App = () =>{
+    const arr = [1,2,3,4,5,6,7,8,9,10]
     return (
         <div className="App">
-            <h1>"helo dio"</h1>
-            <h1>"helo dio"</h1>
+            <h1>HELLO DIO !</h1>
+            <h2>Me chama de hugo</h2>
+            <h1> fala meu saco</h1>
+            <h3>Qume correr é gay</h3>
+            {arr.map(item => (
+                <p>
+                    {item} * 5 = {item * 5}
+                </p>)
+            )}
         </div>
-    )
-}
-export default App
+    );
+};
+
+export default App;
